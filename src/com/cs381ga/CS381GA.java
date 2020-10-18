5f4598f8267cd91e11c77abfa0ad17e74025e457
shmuelhalbfinger <40396822+shmuelhalbfinger@users.noreply.github.com>
2020-10-17 20:38:37 -0400

    Added class with main method, further fixes

--- src/com/cs381ga/CS381GA.java
+++ src/com/cs381ga/CS381GA.java
@@ -2,8 +2,6 @@ package com.cs381ga;
 
 public class CS381GA {
     public static void main(String[] args) {
-        PartitionProblemBruteForce phase1 = new PartitionProblemBruteForce();
-        int [] test = {52,48,65,43,71,70,43,67,54,61};
-        phase1.findPartition(test);
+
     }
 }
