a7f7496cb44cfee56a0f09783648af418b37790d
shmuelhalbfinger <40396822+shmuelhalbfinger@users.noreply.github.com>
2020-10-19 13:05:48 -0400

    Added support for user to enter input and output file names

--- src/com/cs381ga/CS381GA.java
+++ src/com/cs381ga/CS381GA.java
@@ -7,9 +7,9 @@ public class CS381GA {
     public static void main(String[] args) {
         PartitionProblemSolverBruteForce partitionProblemSolverBruteForce = new PartitionProblemSolverBruteForce();
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the name of your input file");
+        System.out.println("Enter the path and name of your input file");
         String inputFile = scanner.nextLine();
-        System.out.println("Enter the name of the file to which you would like to write the output");
+        System.out.println("Enter the path and name of the file to which you would like to write the output");
         String outputFile = scanner.nextLine();
         try {
             partitionProblemSolverBruteForce.solve(inputFile, outputFile, 23);
