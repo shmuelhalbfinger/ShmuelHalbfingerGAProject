bcc5e8dd3d45d7ef0638a4263e0643a4dc76a774
shmuelhalbfinger <40396822+shmuelhalbfinger@users.noreply.github.com>
2020-10-19 12:57:26 -0400

    Added input and output file directory

--- src/com/cs381ga/CS381GA.java
+++ src/com/cs381ga/CS381GA.java
@@ -6,8 +6,8 @@ public class CS381GA {
     public static void main(String[] args) {
         PartitionProblemSolverBruteForce partitionProblemSolverBruteForce = new PartitionProblemSolverBruteForce();
         try {
-            partitionProblemSolverBruteForce.solve("Cs381GaInput.txt",
-                    "Cs381GaOutputBruteForce.txt", 23);
+            partitionProblemSolverBruteForce.solve("Input and Output Files\\Cs381GaInput.txt",
+                    "Input and Output Files\\Cs381GaOutputBruteForce.txt", 23);
         } catch (IOException e) {
             e.printStackTrace();
         }
